5ff730fb6547928184e14b11f7a33156a09efbf5
Chetan Kandarkar <74239460+chetankandarkar@users.noreply.github.com>
2020-12-23 19:39:10 +0530

    Update Arduino1.ino

--- Arduino1.ino
+++ Arduino1.ino
@@ -1,1 +1,52 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+#define in 8
+#define out 9
 
+int count=0;
+
+void setup()
+{
+  lcd.begin(16,2);
+  lcd.print("Visitor Counter");
+  delay(2000);
+  pinMode(in, INPUT);
+  pinMode(out, INPUT);
+  lcd.clear();
+  lcd.print("Person In Room:");
+  lcd.setCursor(0,1);
+  lcd.print(count);
+}
+void loop()
+{  
+  int in_value = digitalRead(in);
+  int out_value = digitalRead(out);
+  if(in_value == LOW)
+  {
+    count++;
+    lcd.clear();
+    lcd.print("Person In Room:");
+    lcd.setCursor(0,1);
+    lcd.print(count);
+    delay(1000);
+  }
+
+  if(out_value == LOW)
+  {
+    count--;
+    lcd.clear();
+    lcd.print("Person In Room:");
+    lcd.setCursor(0,1);
+    lcd.print(count);
+    delay(1000);
+  }
+
+
+  if(count==0)
+  {
+    
+    lcd.clear();
+    lcd.print("Nobody In Room");
+    delay(200);
+  }
+  }
